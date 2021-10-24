810450483343e19e2fccde87b5356a2e80c0d4aa
Amir-R1384 <ranjbaramirreza5786@yahoo.com>
2021-10-24 19:56:11 -0400

    Added 400 error support

--- routes/errorRoutes.js
+++ routes/errorRoutes.js
@@ -4,6 +4,7 @@ const errorController = require('../controllers/errorController')
 
 router.get('/error', errorController.error_get)
 router.get('/500', errorController.error_500_get)
+router.get('/400', errorController.error_400_get)
 router.use(errorController.error_404_get)
 
 module.exports = router
