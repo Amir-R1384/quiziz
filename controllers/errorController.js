13085b1b091c31edb1beab8d4a9e38c7f9265daf
Amir-R1384 <ranjbaramirreza5786@yahoo.com>
2021-11-02 17:43:31 -0400

    Improved error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -3,13 +3,13 @@ module.exports.error_get = (req, res) => {
 }
 
 module.exports.error_500_get = (req, res) => {
-    res.render('error', {layout: 'layouts/blankLayout', title: '500 Error', errorMessage: '500 INTERNAL SERVER ERROR'})
+    res.status(500).render('error', {layout: 'layouts/blankLayout', title: '500 Error', errorMessage: '500 INTERNAL SERVER ERROR'})
 }
 
 module.exports.error_400_get = (req, res) => {
-    res.render('error', {layout: 'layouts/blankLayout', title: '400 Error', errorMessage: '400 BAD REQUEST'})
+    res.status(400).render('error', {layout: 'layouts/blankLayout', title: '400 Error', errorMessage: '400 BAD REQUEST'})
 }
 
 module.exports.error_404_get = (req, res) => {
-    res.render('error', {layout: 'layouts/blankLayout', title: 'Page not found', errorMessage: '404 PAGE NOT FOUND'})
+    res.status(404).render('error', {layout: 'layouts/blankLayout', title: 'Page not found', errorMessage: '404 PAGE NOT FOUND'})
 }
